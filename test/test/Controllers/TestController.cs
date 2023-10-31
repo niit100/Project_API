86de21d9b4fbb68d9ead1942fc437325f2b895e3
unknown <niit.vp02@gmail.com>
2023-10-31 22:02:39 +0530

    Initial commit

---
+++ test/test/Controllers/TestController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestController(/*ICard card*/ IHttpClientFactory httpClientFactory)
+        {
+            //_card = card;
+            _httpClientFactory = httpClientFactory;
+
+        }
+
+        [HttpGet]
+        [Route("FilterdatabasedonNameAndColor")]
+        public async Task<ActionResult<IEnumerable<Card>>> GetCardsAsync([FromQuery] string name, [FromQuery] string color )
+        {
+            try
+            {
+                string apiUrl = "https://api.magicthegathering.io/v1/cards";
+                var queryParameters = new List<string>();
+
+                if (!string.IsNullOrEmpty(name))
+                    queryParameters.Add($"name={Uri.EscapeDataString(name)}");
+
+                if (!string.IsNullOrEmpty(color))
+                    queryParameters.Add($"colors={Uri.EscapeDataString(color)}");
+    
+
+                    if (queryParameters.Count > 0)
+                    apiUrl += "?" + string.Join('&', queryParameters);
+
+                
+                var client = _httpClientFactory.CreateClient();
+                var response = await client.GetStringAsync(apiUrl);
+
+                
+                var apiResponse = JsonSerializer.Deserialize<CardApiResponse>(response, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                if (apiResponse != null && apiResponse.Cards != null)
+                {
+                    
+                    var filteredCards = apiResponse.Cards
+                        .Where(card => IsCardTypeMatch(card, "Creature")) 
+                        .ToList();
+
+                    return Ok(filteredCards);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+               
+                return StatusCode(500, "An error occurred: " + ex.Message);
+            }
+        }
+        bool IsCardTypeMatch(Card card, string desiredType)
+        {
+            if (card.Types != null)
+            {
+                foreach (var type in card.Types)
+                {
+                    if (string.Equals(type, desiredType, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        [HttpGet]
+        [Route("GEtALLData")]
+        public async Task<ActionResult<IEnumerable<Card>>> GetCardsdataAsync()
+        {
+            try
+            {
+                string apiUrl = "https://api.magicthegathering.io/v1/cards";
+                var queryParameters = new List<string>();
+                var client = _httpClientFactory.CreateClient();
+                var response = await client.GetStringAsync(apiUrl);
+
+              
+                var apiResponse = JsonSerializer.Deserialize<CardApiResponse>(response, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                if (apiResponse != null && apiResponse.Cards != null && apiResponse.Cards.Any())
+                {
+                    return Ok(apiResponse.Cards);
+                }
+                else
+                {
+                    return NotFound("No cards found for the specified criteria.");
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, "An error occurred: " + ex.Message);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ test/test/MtgCard.cs
@@ -0,0 +1,103 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Linq;
+
+namespace test
+{
+    public class Card
+    {
+        public string Name { get; set; }
+        public string ManaCost { get; set; }
+        public double CMC { get; set; }
+        public List<string> Colors { get; set; }
+        public List<string> ColorIdentity { get; set; }
+        public string Type { get; set; }
+        public List<string> Types { get; set; }
+        public List<string> Subtypes { get; set; }
+        public string Rarity { get; set; }
+        public string Set { get; set; }
+        public string SetName { get; set; }
+        public string Text { get; set; }
+        public string Artist { get; set; }
+        public string Number { get; set; }
+        public string Power { get; set; }
+        public string Toughness { get; set; }
+        public string Layout { get; set; }
+        [JsonPropertyName("multiverseid")]
+        public string MultiverseID { get; set; }
+        public string ImageUrl { get; set; }
+        public List<ForeignName> foreignNames { get; set; }
+    }
+
+    public class ForeignName
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public string Type { get; set; }
+        public string Flavor { get; set; }
+        public string ImageUrl { get; set; }
+        public string Language { get; set; }
+        public int? MultiverseID { get; set; }
+    }
+    public class CardApiResponse
+    {
+        public List<Card> Cards { get; set; }
+        
+    }
+
+
+    public class CardLanguageConverter : JsonConverter<Card>
+    {
+        public override Card Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            // Create a dictionary to map language names to deserialized Card objects
+            var languageToCardMap = new Dictionary<string, Card>();
+
+            using (JsonDocument doc = JsonDocument.ParseValue(ref reader))
+            {
+                var root = doc.RootElement;
+
+                // Loop through the foreignNames array and extract language-specific information
+                foreach (var foreignNameElement in root.GetProperty("foreignNames").EnumerateArray())
+                {
+                    string language = foreignNameElement.GetProperty("language").GetString();
+                    var card = JsonSerializer.Deserialize<Card>(foreignNameElement.GetRawText());
+
+                    if (!languageToCardMap.ContainsKey(language))
+                    {
+                        // Handle multiverseid conversion to integer or null
+                        var multiverseidProperty = foreignNameElement.GetProperty("multiverseid");
+                        if (multiverseidProperty.ValueKind == JsonValueKind.Number)
+                        {
+                            card.MultiverseID = multiverseidProperty.GetInt32().ToString();
+                        }
+                        else
+                        {
+                            card.MultiverseID = null; // or set to a default value
+                        }
+
+                        languageToCardMap[language] = card;
+                    }
+                }
+
+            }
+
+            // Here, you can return the desired language's Card object
+            if (languageToCardMap.ContainsKey("English"))
+            {
+                return languageToCardMap["English"];
+            }
+
+            // Handle unsupported languages or return a default Card if needed
+            return null;
+        }
+
+        public override void Write(Utf8JsonWriter writer, Card value, JsonSerializerOptions options)
+        {
+            // Implement write if necessary
+        }
+    }
+
+
+}
+
